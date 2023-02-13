00ab5658ccf47eb514391fccdb9f85e8d75dfcbe
artemyr <artem.r@affetta.ru>
2023-02-13 15:33:11 +0300

    xxx

--- local/components/custom/wishlist/class.php
+++ local/components/custom/wishlist/class.php
@@ -7,8 +7,6 @@ class WishlistComponent extends CBitrixComponent
 {
     public function executeComponent()
     {
-        global $USER;
-        global $APPLICATION;
         CModule::IncludeModule("sale");
 
         $arBasketItems = $this->authFavs();
@@ -154,64 +152,5 @@ class WishlistComponent extends CBitrixComponent
             $arBasketItems[] = $arItems;
         }
         return $arBasketItems;
-    }
-
-    private function nonAuthFavs(){
-        global $APPLICATION;
-
-        if (isset($_REQUEST[$this->arParams['ACTION_VARIABLE']]) && isset($_REQUEST[$this->arParams['PRODUCT_ID_VARIABLE']])) {
-            $successfulAction = true;
-            $findMethod = true;
-            $actionMessage = '';
-            $actionByAjax = isset($_REQUEST['ajax_action']) && $_REQUEST['ajax_action'] == 'Y';
-
-            $productID = (int)$_REQUEST[$this->arParams['PRODUCT_ID_VARIABLE']];
-            $resultCount = 0;
-            if ($productID > 0) {
-                switch (ToUpper($_REQUEST[$this->arParams['ACTION_VARIABLE']])) {
-                    case 'ADD_TO_WISHLIST_LIST':
-                        $actionMessage = "add to wishlist";
-                        if (!isset($_SESSION[$this->arParams["NAME"]][$this->arParams["IBLOCK_ID"]]["ITEMS"][$productID]))
-                        {
-                            $_SESSION[$this->arParams['NAME']][$this->arParams['IBLOCK_ID']]['ITEMS'][$productID] = array(
-                                'ID' => $productID,
-                                'DELETE_URL' => htmlspecialcharsbx($APPLICATION->GetCurPageParam(
-                                    $this->arParams['ACTION_VARIABLE'] . "=DELETE_FROM_COMPARE_LIST&" . $this->arParams['PRODUCT_ID_VARIABLE'] . "=" . $productID,
-                                    array($this->arParams['ACTION_VARIABLE'], $this->arParams['PRODUCT_ID_VARIABLE'])
-                                ))
-                            );
-                            unset($sectionsList, $arElement);
-                            $resultCount = count($_SESSION[$this->arParams['NAME']][$this->arParams['IBLOCK_ID']]['ITEMS']);
-                        }
-                        break;
-                    case 'DELETE_FROM_WISHLIST_LIST':
-                        if (isset($_SESSION[$this->arParams["NAME"]][$this->arParams["IBLOCK_ID"]]["ITEMS"][$productID]))
-                            unset($_SESSION[$this->arParams["NAME"]][$this->arParams["IBLOCK_ID"]]["ITEMS"][$productID]);
-                        $actionMessage = "remove from wishlist";
-                        $resultCount = count($_SESSION[$this->arParams["NAME"]][$this->arParams["IBLOCK_ID"]]["ITEMS"]);
-                        break;
-                    default:
-                        $findMethod = false;
-                        break;
-                }
-            } else {
-                $successfulAction = false;
-                $actionMessage = GetMessage('CP_BCCL_ERR_MESS_PRODUCT_NOT_FOUND');
-            }
-
-            if ($actionByAjax && $findMethod) {
-                if ($successfulAction)
-                    $addResult = array('STATUS' => 'OK', 'MESSAGE' => $actionMessage, 'ID' => $productID, 'COUNT' => $resultCount);
-                else
-                    $addResult = array('STATUS' => 'ERROR', 'MESSAGE' => $actionMessage);
-
-                $APPLICATION->RestartBuffer();
-                header('Content-Type: application/json');
-                echo Main\Web\Json::encode($addResult);
-                die();
-            }
-        }
-
-        return $_SESSION[$this->arParams["NAME"]][$this->arParams["IBLOCK_ID"]]["ITEMS"];
     }
 }
